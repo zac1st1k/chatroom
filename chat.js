dc4d50dcbf9233827034b832cb661e95b8101129
zac1st1k <zacfirst@gmail.com>
2015-11-06 01:28:39 +1100

    + Fix for JSHint

--- chat.js
+++ chat.js
@@ -1,20 +1,32 @@
-var net = require('net')
+var net = require('net');
 var chatServer = net.createServer(),
-    clientList = []
+    clientList = [];
 chatServer.on('connection', function(client) {
-    client.name = client.remoteAddress + ':' + client.remotePort
+    'use strict';
+    client.name = client.remoteAddress + ':' + client.remotePort;
     client.write('Hi ' + client.name + '!\n');
-    clientList.push(client)
+    clientList.push(client);
     client.on('data', function(data) {
-        broadcast(data, client)
-    })
-})
+        broadcast(data, client);
+    });
+    client.on('end', function() {
+        clientList.splice(clientList.indexOf(client), 1);
+    });
+});
+
 
 function broadcast(message, client) {
+    'use strict';
+    var cleanup = [];
     for (var i = 0; i < clientList.length; i += 1) {
         if (client !== clientList[i]) {
-            clientList[i].write(client.name + " says " + message)
+            if (clientList[i].writable) {
+                clientList[i].write(client.name + ' says ' + message);
+            } else {
+                cleanup.push(clientList[i]);
+                clientList[i].destroy();
+            }
         }
     }
 }
-chatServer.listen(9000)
+chatServer.listen(9000);
