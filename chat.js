ee3735638d5560ff3569ea999d02378c2d6a07ea
zac1st1k <zacfirst@gmail.com>
2015-11-05 23:20:04 +1100

    + listen all data

--- chat.js
+++ chat.js
@@ -1,8 +1,9 @@
  var net = require('net')
-    var chatServer = net.createServer()
-    chatServer.on('connection', function(client) {
-      client.write('Hi!\n');
-      client.write('Bye!\n');
-      client.end()
-    })
-    chatServer.listen(9000)
+ var chatServer = net.createServer()
+ chatServer.on('connection', function(client) {
+     client.write('Hi!\n');
+     client.on('data', function(data) {
+         console.log(data)
+     })
+ })
+ chatServer.listen(9000)
