92898c75c8559436dd0f84c8b0c0d4f3bf9ec87b
zac1st1k <zacfirst@gmail.com>
2015-11-05 23:40:30 +1100

    + Multiple clients

--- chat.js
+++ chat.js
@@ -1,9 +1,13 @@
- var net = require('net')
- var chatServer = net.createServer()
- chatServer.on('connection', function(client) {
-     client.write('Hi!\n');
-     client.on('data', function(data) {
-         console.log(data)
-     })
- })
- chatServer.listen(9000)
+var net = require('net');
+var chatServer = net.createServer();
+var clientList = [];
+chatServer.on('connection', function(client) {
+    client.write('Hi!\n');
+    clientList.push(client)
+    client.on('data', function(data) {
+        for (var i = 0; i < clientList.length; i += 1) {
+            clientList[i].write(data)
+        }
+    })
+})
+chatServer.listen(9000);
